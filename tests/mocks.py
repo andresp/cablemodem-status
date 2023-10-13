1040aa592fecd989fc4f220dc8ddfaca6b1a7fa2
Andy Petralli <6251451+andresp@users.noreply.github.com>
2023-10-13 14:57:58 -0700

    Cleanup mock config (#22)

---
+++ tests/mocks.py
@@ -0,0 +1,17 @@
+config = {
+    'Database': {
+        'Bucket': 'testBucket',
+        'Org': 'org',
+        'Host': 'localhost',
+        'Port': '443',
+        'UseTls': True,
+        'Token': 'token'
+    },
+    'General': {
+        'HostTimezone': 'Pacific/Los_Angeles'
+    },
+    'Modem': {
+        'Host': '10.0.0.1',
+        'LogTimezone': 'Pacific/Los_Angeles'
+    }
+}
--- tests/test_motorolamb8600m.py
+++ tests/test_motorolamb8600m.py
@@ -3,30 +3,13 @@ from docsismodem.modems.motorola_mb8600 import MotorolaMB8600
 from docsismodem.modems.observablemodem import ObservableModem
 from docsismodem.modems.observablemodemfactory import ObservableModemFactory
 
+from tests.mocks import config
 
 class TestMotorolaMB8600:
 
-    mockConfig = {
-        'Database': {
-            'Bucket': 'testBucket',
-            'Org': 'org',
-            'Host': 'localhost',
-            'Port': '443',
-            'UseTls': True,
-            'Token': 'token'
-        },
-        'General': {
-            'HostTimezone': 'Pacific/Los_Angeles'
-        },
-        'Modem': {
-            'Host': '10.0.0.1',
-            'LogTimezone': 'Pacific/Los_Angeles'
-        }
-    }
-
     def test_init(self):
         
-        instance = ObservableModemFactory.get("MotorolaMB8600", self.mockConfig, logging.getLogger(None))
+        instance = ObservableModemFactory.get("MotorolaMB8600", config, logging.getLogger(None))
         assert isinstance(instance, ObservableModem)
         assert type(instance) is MotorolaMB8600
 
