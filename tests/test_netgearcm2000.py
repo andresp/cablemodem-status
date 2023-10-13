55d652f4a3990eba4c7d8464c4cc755d00e5633b
Andres Petralli <6251451+andresp@users.noreply.github.com>
2023-10-13 21:56:00 +0000

    Cleanup mock config

--- tests/test_netgearcm2000.py
+++ tests/test_netgearcm2000.py
@@ -3,30 +3,13 @@ from docsismodem.modems.netgear_cm2000 import NetgearCM2000
 from docsismodem.modems.observablemodem import ObservableModem
 from docsismodem.modems.observablemodemfactory import ObservableModemFactory
 
+from tests.mocks import config
 
 class TestNetgear2000:
 
-    mockConfig = {
-        'Database': {
-            'Bucket': 'testBucket',
-            'Org': 'org',
-            'Host': 'localhost',
-            'Port': '443',
-            'UseTls': True,
-            'Token': 'token'
-        },
-        'General': {
-            'HostTimezone': 'Pacific/Los_Angeles'
-        },
-        'Modem': {
-            'Host': '10.0.0.1',
-            'LogTimezone': 'Pacific/Los_Angeles'
-        }
-    }
-
     def test_init(self):
         
-        instance = ObservableModemFactory.get("NetgearCM2000", self.mockConfig, logging.getLogger(None))
+        instance = ObservableModemFactory.get("NetgearCM2000", config, logging.getLogger(None))
         assert isinstance(instance, ObservableModem)
         assert type(instance) is NetgearCM2000
 
